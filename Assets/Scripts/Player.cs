c6df24790e3472b3e60eeb3b4d45ea3657600e6d
Vikas Arora <vikasarora200@gmail.com>
2025-12-31 01:42:03 +0530

    adds ability to pickup kitchen objects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,7 @@
 using System;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IKitchenObjectParent
 {
 	public static Player Instance { get; private set; }
 	public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
@@ -13,9 +13,11 @@ public class Player : MonoBehaviour
 	[SerializeField] private float moveSpeed = 7f;
 	[SerializeField] private GameInput gameInput;
 	[SerializeField] private LayerMask countersLayerMask;
+	[SerializeField] private Transform kitchenObjectHoldPoint;
 	private bool isWalking;
 	private Vector3 lastInteractDir;
 	private ClearCounter selectedCounter;
+	private KitchenObject kitchenObject;
 
 	private void Awake()
 	{
@@ -40,7 +42,7 @@ public class Player : MonoBehaviour
 	{
 		if (selectedCounter != null)
 		{
-			selectedCounter.Interact();
+			selectedCounter.Interact(this);
 		}
 	}
 
@@ -141,5 +143,30 @@ public class Player : MonoBehaviour
 		{
 			selectedCounter = selectedCounter
 		});
+	}
+
+	public Transform GetKitchenObjectFollowTransform()
+	{
+		return kitchenObjectHoldPoint;
+	}
+
+	public void SetKitchenObject(KitchenObject kitchenObject)
+	{
+		this.kitchenObject = kitchenObject;
+	}
+
+	public KitchenObject GetKitchenObject()
+	{
+		return kitchenObject;
+	}
+
+	public void ClearKitchenObject()
+	{
+		kitchenObject = null;
+	}
+
+	public bool HasKitchenObject()
+	{
+		return kitchenObject != null;
 	}
 }
