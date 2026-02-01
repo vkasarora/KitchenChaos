75824d2836248b2bf10bb9b1625a6927cd4b1f3b
Vikas Arora <vikasarora200@gmail.com>
2026-02-01 23:13:45 +0530

    Map setup and initial delivery setup

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                // Only accept plates
+                player.GetKitchenObject().DestroySelf();
+            }
+
+
+        }
+    }
+}
