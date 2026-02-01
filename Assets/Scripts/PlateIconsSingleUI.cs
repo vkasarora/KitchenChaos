527de36bb97010b22dade99b97c6389ed210949c
Vikas Arora <vikasarora200@gmail.com>
2026-02-01 22:41:18 +0530

    Adds logic for kitchen ingredient icons in canvas UI

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,12 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlateIconsSingleUI : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO)
+    {
+        image.sprite = kitchenObjectSO.sprite;
+    }
+}
