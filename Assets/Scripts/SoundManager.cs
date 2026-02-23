a1ce1a9c1b237dad0efc6789cddfbd7b78368062
Vikas Arora <vikasarora200@gmail.com>
2026-02-24 01:15:54 +0530

    Music and sfx manager with state saves

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -6,10 +6,15 @@ public class SoundManager : MonoBehaviour
     public static SoundManager Instance { get; private set; }
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
+    private float volume = 1f;
+    private const string PLAYER_PREFS_SFX_VOLUME = "SfxVolume"; 
+
 
     public void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SFX_VOLUME, 1f);
     }
 
     private void Start()
@@ -62,13 +67,31 @@ public class SoundManager : MonoBehaviour
         PlaySound(audioClipArray[UnityEngine.Random.Range(0, audioClipArray.Length)], position, volume);
     }
 
-    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
     {
-        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 
     public void PlayFootstepsSound(Vector3 position, float volume)
     {
         PlaySound(audioClipRefsSO.footstep, position, volume);
     }
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SFX_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+    public float GetVolume()
+    {
+        return volume;
+    }
+
 }
---
+++ Assets/Scripts/UI/OptionsUI.cs
@@ -0,0 +1,64 @@
+using System;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsUI : MonoBehaviour
+{
+    public static OptionsUI Instance { get; private set; }
+    [SerializeField] private Button soundEffectButton;
+    [SerializeField] private Button musicButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private TextMeshProUGUI soundEffectsText;
+    [SerializeField] private TextMeshProUGUI musicText;
+
+    private void Awake()
+    {
+        Instance = this;
+        soundEffectButton.onClick.AddListener(() =>
+        {
+            SoundManager.Instance.ChangeVolume();
+            UpdateVisual();
+        });
+
+        musicButton.onClick.AddListener(() =>
+        {
+            MusicManager.Instance.ChangeVolume();
+            UpdateVisual();
+        });
+
+        closeButton.onClick.AddListener(() =>
+        {
+           Hide(); 
+        });
+    }
+
+    private void Start()
+    {
+        GameManager.Instance.OnGameUnpaused += GameManager_OnGameUnpaused;
+        UpdateVisual();
+        Hide();
+    }
+
+    private void GameManager_OnGameUnpaused(object sender, EventArgs e)
+    {
+        Hide();
+    }
+
+    private void UpdateVisual()
+    {
+        soundEffectsText.text = "Sound Effects: " + Mathf.Round(SoundManager.Instance.GetVolume() * 10f);
+        musicText.text = "Music: " + Mathf.Round(MusicManager.Instance.GetVolume() * 10f);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
